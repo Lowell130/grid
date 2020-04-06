fac7b7b847bcf3dcd7bc2f4c0102a3f2fa623730
Lowell130 <ghost.pizza@gmail.com>
2020-04-06 17:19:01 +0200

    Update main.js

--- my-shop/src/main.js
+++ my-shop/src/main.js
@@ -2,15 +2,6 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
-import '~/assets/css/bootstrap.css'
-import '~/assets/fonts/fontawesome/css/all.min.css'
-import '~/assets/css/responsive.css'
-import '~/assets/css/ui.css'
-
-
-
-
-
 
 
 export default function (Vue, { router, head, isClient }) {
